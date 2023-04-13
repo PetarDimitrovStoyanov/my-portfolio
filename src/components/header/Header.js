769c02c3502de7b6f6b1079729dd047cbbc3a785
Petar Stoyanov <59773142+PetarDimitrovStoyanov@users.noreply.github.com>
2023-04-14 00:33:37 +0300

    created Header section

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,34 @@
 import React from 'react';
-import './Header.css';
+import './Header.scss';
 
 export default function Header() {
     return (
-        <div>
-            Header
-        </div>
+        <section className="header">
+            <div className="container">
+                <nav className="navigation">
+                    <div className="logo">
+                        <p className="portfolio"> Portfolio </p>
+                        <span className="name">Petar Stoyanov</span>
+                    </div>
+
+                    <ul>
+                        <li className="list-item"><a className="nav-link" href="">Home</a></li>
+                        <li className="list-item"><a className="nav-link" href="">About</a></li>
+                        <li className="list-item"><a className="nav-link" href="">Certificates</a></li>
+                        <li className="list-item"><a className="nav-link" href="">Portfolio</a></li>
+                        <li className="list-item"><a className="nav-link" href="">Contact</a></li>
+                    </ul>
+                </nav>
+                <div className="header-text">
+                    <div>
+                        <span className="special big">&lt;</span>
+                        <span>Fullstack developer</span>
+                        <span className="special small">&#47;</span>
+                        <span className="special big">&gt;</span>
+                    </div>
+                    <h1>Hello, <br/> I'm <span className="special">Petar</span> Stoyanov</h1>
+                </div>
+            </div>
+        </section>
     );
 }
