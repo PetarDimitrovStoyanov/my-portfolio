d577b2a3c047d8c44e61b06cc006bae68ccc380b
Petar Stoyanov <59773142+PetarDimitrovStoyanov@users.noreply.github.com>
2023-04-14 17:30:04 +0300

    created quote and footer. Finished layout for the contact form

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Footer.scss';
+
+export default function Footer (){
+    return (
+        <section className="footer">
+            <p className="copyright">Copyright © Made by Petar Stoyanov</p>
+        </section>
+    );
+}
---
+++ src/components/quote/Quote.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Quote.scss';
+
+export default function Quote (){
+    return (
+        <section className="quote">
+            <p className="text">HARD WORK BEATS TALENT WHEN TALENT DOESN’T WORK HARD.</p>
+        </section>
+    );
+}
