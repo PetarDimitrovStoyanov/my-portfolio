a749e309592c080dda579e87b7a5b830b47d4305
Petar Stoyanov <59773142+PetarDimitrovStoyanov@users.noreply.github.com>
2023-04-18 03:09:14 +0300

    Fixed submitting form. Changed "About Me" image and some CSS properties

--- src/sections/about/About.js
+++ src/sections/about/About.js
@@ -17,12 +17,12 @@ export default function About() {
         <section className="about" id="about-me">
             <div className="container">
                 <div className="row">
-                    <div className="about-col-1 desktop">
+                    <div className="about-col-1 desktop img-container-layer">
                         <img src={require('../../assets/official-hd.png')} alt="user"/>
                     </div>
                     <div className="about-col-2">
                         <h1 className="sub-title">{t('header.about')}</h1>
-                        <div className="about-col-1 small">
+                        <div className="about-col-1 small img-container-layer">
                             <img src={require('../../assets/official-hd.png')} alt="user"/>
                         </div>
                         <p>{t('about-me.description1')}
@@ -34,7 +34,8 @@ export default function About() {
                             {t('about-me.description4')}
                         </p>
                         <div className="tab-titles">
-                            <p className="tab-links active-link skills" onClick={ () => open('skills')}>{t('about-me.skills')}</p>
+                            <p className="tab-links active-link skills" onClick={ () => open('skills')}>{t('about-me.tech-skills')}</p>
+                            <p className="tab-links soft-skills" onClick={ () => open('soft-skills')}>{t('about-me.soft-skills')}</p>
                             <p className="tab-links experience" onClick={ () => open('experience')}>{t('about-me.experience')}</p>
                             <p className="tab-links education" onClick={ () => open('education')}>{t('about-me.education')}</p>
                         </div>
@@ -47,9 +48,20 @@ export default function About() {
                                 <li className="tab-content-items"><span>{t('about-me.skill1.working-database')}
                                     - MySQL {t('about-me.skill1.and')} MongoDB</span></li>
                                 <li className="tab-content-items"><span>Source control system - Git</span></li>
-                                <li className="tab-content-items"><span>Docker</span></li>
-                                <li className="tab-content-items"><span>Kubernetes ({t('about-me.skill1.beginner')})</span></li>
+                                <li className="tab-content-items"><span>Kubernetes ({t('about-me.skill1.beginner')}) and Docker</span></li>
+                                <li className="tab-content-items"><span>{t('about-me.skill1.microservice')} ({t('about-me.skill1.beginner')})</span></li>
                                 <li className="tab-content-items"><span>DevOps {t('about-me.skill1.with')} AWS ({t('about-me.skill1.beginner')})</span></li>
+                            </ul>
+                        </div>
+                        <div className="tab-contents soft-skills">
+                            <ul>
+                                <li className="tab-content-items"><span>{t('about-me.soft-skills1.communication')}</span></li>
+                                <li className="tab-content-items"><span>{t('about-me.soft-skills1.critical')}</span></li>
+                                <li className="tab-content-items"><span>{t('about-me.soft-skills1.pressure')}</span></li>
+                                <li className="tab-content-items"><span>{t('about-me.soft-skills1.ethic')}</span></li>
+                                <li className="tab-content-items"><span>{t('about-me.soft-skills1.multitasking')}</span></li>
+                                <li className="tab-content-items"><span>{t('about-me.soft-skills1.persist')}</span></li>
+                                <li className="tab-content-items"><span>{t('about-me.soft-skills1.fast')}</span></li>
                             </ul>
                         </div>
                         <div className="tab-contents experience">
