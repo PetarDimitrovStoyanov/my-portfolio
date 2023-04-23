aced5ae3fb9cac147def12b7f1caa59301666525
Petar Stoyanov <59773142+PetarDimitrovStoyanov@users.noreply.github.com>
2023-04-23 15:02:07 +0300

    Added translations, and media queries for zooming.

--- src/components/sections/footer/Footer.js
+++ src/components/sections/footer/Footer.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import './Footer.scss';
+import {useLocation} from "react-router-dom";
 
 export default function Footer (){
+    const location = useLocation();
+
+    function isNotFoundPage() {
+       return !location.pathname.includes('/projects') && location.pathname.length > 1
+    }
+
     return (
-        <section className="footer position">
+        <section className={`footer position ${isNotFoundPage() ? 'not-found-page' : ''}`}>
             <p className="copyright">Copyright © Made by Petar Stoyanov</p>
         </section>
     );
--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,17 +1,19 @@
 import {useTranslation} from 'react-i18next';
 import React from 'react';
 import './NotFound.scss';
+import {useNavigate} from "react-router-dom";
 
 export default function NotFound() {
     const { t } = useTranslation();
+    const navigate = useNavigate();
 
     return (
         <section className="not-found-container">
             <div className="message">
                 <p className="status-code">404</p>
-                <h2 className="text">Sorry, the page you tried to reach is not found.</h2>
+                <h2 className="text">{t('not-found.message')}</h2>
             </div>
-            <button className="button">Back to the home page</button>
+            <button className="button" onClick={() => navigate('/')}>{t('not-found.back')}</button>
         </section>
     );
 }
